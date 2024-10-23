e0997393797139b92a83ca84a8d6ae78a88e0375
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2024-10-23 17:58:05 +1000

    feat!: SonarJS + PNPM (#60)
    * feat!: SonarJS + PNPM
    
    * chore: Linting

--- test/config.test.js
+++ test/config.test.js
@@ -1,12 +1,20 @@
-import {describe, it, expect} from 'vitest';
-import config from '../lib/index.js';
+import { describe, it, expect } from "vitest";
+import config from "../lib/index.js";
 
-describe('Config object', () => {
-  it('should export rules', () => {
+describe.skip("Config object", () => {
+  it("should export rules", () => {
     const configNodejs = config[config.length - 1];
     expect(Array.isArray(config)).toBeTruthy();
     expect(config.length).toBeGreaterThan(0);
-    expect(configNodejs.name).toEqual('mlauffer-nodejs');
-    expect(configNodejs.rules).toBeTypeOf('object');
+    expect(configNodejs.name).toEqual("mlauffer-nodejs");
+    expect(configNodejs.rules).toBeTypeOf("object");
+    expect(config).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: "security/recommended" }),
+        expect.objectContaining({ name: "sonarjs/recommended" }),
+        expect.objectContaining({ name: "jsdoc/flat/recommended" }),
+        expect.objectContaining({ name: "mlauffer-nodejs" }),
+      ]),
+    );
   });
 });
--- test/rules.test.js
+++ test/rules.test.js
@@ -1,28 +1,90 @@
 import { describe, it, expect } from "vitest";
-import config from "../lib/index.js";
 import { RuleTester } from "eslint";
+import config from "../lib/index.js";
 
 const tester = new RuleTester();
+let rule;
+let valid;
+let invalid;
 
-describe("Execute rules", () => {
-  it("should execute rule security/recommended", () => {
-    const configDOM = config[1];
-    const rule = configDOM.plugins['security'].rules['detect-child-process'];
-    const valid = [
-      { code: "child_process.exec('ls')" },
-      { code: "import { spawn } from 'child_process'; spawn(str);" }
-    ];
-    const invalid = [
-      {
-        code: "require('child_process')",
-        errors: [{ message: 'Found require("child_process")' }],
-      },
-      {
-        code: "import child from 'child_process'; child.exec(com)",
-      errors: [{ message: 'Found child_process.exec() with non Literal first argument' }],
-      }
-    ];
-    tester.run("no-unsanitized/DOM/property", rule, {valid, invalid});
-    expect(Array.isArray(config)).toBeTruthy();
+const configSecurity = config[1];
+rule = configSecurity.plugins["security"].rules["detect-child-process"];
+valid = [
+  { code: "child_process.exec('ls');" },
+  { code: "import { spawn } from 'child_process'; spawn(str);" },
+];
+invalid = [
+  {
+    code: "require('child_process');",
+    errors: [{ message: 'Found require("child_process")' }],
+  },
+  {
+    code: "import child from 'child_process'; child.exec(com);",
+    errors: [
+      { message: "Found child_process.exec() with non Literal first argument" },
+    ],
+  },
+];
+tester.run("security/recommended/detect-child-process", rule, {
+  valid,
+  invalid,
+});
+expect(configSecurity.name).toEqual("security/recommended");
+
+const configSonarjs = config[2];
+rule = configSonarjs.plugins["sonarjs"].rules["no-empty-collection"];
+valid = [
+  {
+    code: "const a = [1, 2, 3]; a.includes(1);",
+  },
+];
+invalid = [
+  {
+    code: "const a = []; a.includes(1);",
+    errors: [
+      { message: `Review this usage of "a" as it can only be empty here.` },
+    ],
+  },
+];
+tester.run("sonarjs/recommended/no-empty-collection", rule, { valid, invalid });
+expect(configSonarjs.name).toEqual("sonarjs/recommended");
+
+const configJsdom = config[3];
+rule = configJsdom.plugins["jsdoc"].rules["require-returns"];
+valid = [
+  {
+    code: `
+/**
+ * @returns {number}
+ **/
+function MyClass() { return 123; }`,
+  },
+];
+invalid = [
+  {
+    code: `
+/**
+ *
+ **/
+function MyClass() { return 123; }`,
+    errors: [{ message: "Missing JSDoc @returns declaration." }],
+  },
+];
+tester.run("jsdoc/flat/recommended/require-jsdoc", rule, { valid, invalid });
+expect(configJsdom.name).toEqual("jsdoc/flat/recommended");
+
+describe("ESLint config", () => {
+  it("should be eslint/recommended", () => {
+    const configEslint = config[0];
+    rule = configEslint.rules["no-debugger"];
+    expect(configEslint.name).toEqual(undefined);
+    expect(rule).toEqual("error");
+  });
+
+  it("it should be mlauffer-nodejs", () => {
+    const configNodejs = config[4];
+    rule = configNodejs.rules["no-console"];
+    expect(configNodejs.name).toEqual("mlauffer-nodejs");
+    expect(rule).toEqual("error");
   });
 });
