718ae348c7612050f7daa3de86d043cc3aaf20ae
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-03-17 14:42:30 +1000

    feat: Use defineConfig + Extends

--- test/config.test.js
+++ test/config.test.js
@@ -3,16 +3,19 @@ import config from "../lib/index.js";
 
 describe("Config object", () => {
   it("should export rules", () => {
-    const configNodejs = config[config.length - 1];
     expect(Array.isArray(config)).toBeTruthy();
-    expect(config.length).toBeGreaterThan(0);
-    expect(configNodejs.name).toEqual("mlauffer-nodejs");
-    expect(configNodejs.rules).toBeTypeOf("object");
+    expect(config.length).toEqual(5);
     expect(config).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ name: "security/recommended" }),
-        expect.objectContaining({ name: "sonarjs/recommended" }),
-        expect.objectContaining({ name: "jsdoc/flat/recommended" }),
+        expect.objectContaining({
+          name: expect.stringMatching(/security\/recommended$/),
+        }),
+        expect.objectContaining({
+          name: expect.stringMatching(/sonarjs\/recommended$/),
+        }),
+        expect.objectContaining({
+          name: expect.stringMatching(/jsdoc\/flat\/recommended$/),
+        }),
         expect.objectContaining({ name: "mlauffer-nodejs" }),
       ]),
     );
